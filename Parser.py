333d4a82c884d53c405202d1575ac86e8b438f0e
mynameisvinn <vin.tang@gmail.com>
2017-07-02 22:04:05 -0400

    pylint cleanup

--- Parser.py
+++ Parser.py
@@ -4,23 +4,23 @@ from spacy.en import English
 
 def read_email(fname):
     """Read email as unicode."""
-    with codecs.open(fname, encoding='utf-8') as f:
-        text = f.read().encode('utf-8')
+    with codecs.open(fname, encoding='utf-8') as email:
+        text = email.read().encode('utf-8')
     return text
 
 def strip(msg):
     return msg.strip().split('\n')
 
-def generate_text(msg, threshold, POS_parser, fname):
+def generate_text(sentences, threshold, pos_parser, fname):
     """
-    For each line, determine if line is signature block. 
+    For each line, determine if line is signature block.
 
     If prob(signature block) is less than the defined threshold, write
     line to file.
 
     Parameters
     ----------
-    msg : str
+    sentence : str
         Represents line in email block.
     threshold: float
         Lower thresholds will result in more false positives.
@@ -28,21 +28,20 @@ def generate_text(msg, threshold, POS_parser, fname):
         Spacy English object used to tag parts-of-speech. Will explore using
         other POS taggers like NLTK's.
     """
-    with open(fname, "w") as f:
-        for m in msg:
-            if _prob_block(m, POS_parser) < threshold:
-                f.write(unicode(m, 'utf8'))
-    print "complete"
+    with open(fname, "w") as new_file:
+        for sentence in sentences:
+            if _prob_block(sentence, pos_parser) < threshold:
+                new_file.write(unicode(sentence, 'utf8'))
     return True
 
-def _prob_block(multiSentence, POS_parser):
+def _prob_block(sentence, pos_parser):
     """Calculate probability of email block.
 
     Parameters
     ----------
-    multiSentence : str
+    sentence : str
         Line in email block.
-    POS_parser : obj
+    pos_parser : obj
         A POS tagger object. This version relies on Spacy's English POS-tagger.
 
     Returns
@@ -50,12 +49,15 @@ def _prob_block(multiSentence, POS_parser):
     probability(signature block | line)
     """
     try:
-        multiSentence = unicode(multiSentence)
-        parsedData = POS_parser(multiSentence)
-        for span in parsedData.sents:
-            sent = [parsedData[i] for i in range(span.start, span.end)]
+        sentence = unicode(sentence)
+        parsed_data = pos_parser(sentence)
+        for span in parsed_data.sents:
+            sent = [parsed_data[i] for i in range(span.start, span.end)]
         non_verbs = np.sum([token.pos_ != 'VERB' for token in sent])
         total = len(sent)
         return float(non_verbs) / total
     except:
         return 0
+
+if __name__ == '__main__':
+    pass
